4e09a495f264e258aed60de519395c99fe3f0510
RQuicho <raymondquicho@gmail.com>
2023-08-20 15:51:47 -0700

    completing part 2

--- urls.js
+++ urls.js
@@ -1,41 +1,45 @@
 const fs = require('fs');
 const axios = require('axios');
-
-
-const writeFile = (data) => {
-    fs.writeFile("rithmschool.com", data, 'utf8', (err) => {
-        if (err) {
-            console.log(err);
-            process.kill(1);
-        }
-        console.log('Wrote to rithmschool.com');
-    })
-}
+const sanitize = require('sanitize-filename');
 
 
 const urls = (path) => {
     fs.readFile(path, 'utf8', (err, data) => {
         if (err) {
             console.log(err);
-            process.kill(1);
+            process.exit(1);
         }
-        console.log(data);
+        // console.log(data); // output: four urls in urls.txt file
+        const urlList = data.trim().split('\n'); // creates an array of the four urls
+        for (let url of urlList) {
+            webUrl(url);
+        }
     });
-    console.log(data);
-}
-// urls(process.argv[2]); // run: node urls.js urls.txt
+};
+urls(process.argv[2]); // run: node urls.js urls.txt
 
+
+const writeFile = (fileName, data) => {
+    fs.writeFile(fileName, data, 'utf8', (err) => {
+        if (err) {
+            console.log(err);
+            process.exit(1);
+        }
+        console.log(`Wrote to ${fileName}`);
+    });
+};
 
 
 async function webUrl (url) {
     try {
         let res = await axios.get(url);
-        // console.log(res.data);
-        writeFile(res.data);
+        const fileName = url.replace(/^(https?:\/\/)?/i, '').replace(/[^a-zA-Z0-9-_]/g, '.');
+        writeFile(fileName, res.data);
     } catch (err) {
-        console.log(err);
-        process.kill(1);
+        console.log(`Couldn't download ${url}`);
+        // console.log(err);
+        // process.exit(1);
     }
-}
-webUrl(process.argv[2]); // run: node urls.js http://rithmschool.com
+};
+// webUrl(process.argv[2]); // run: node urls.js http://rithmschool.com
 
